7a811532b64a1121a58650766643ea92b86bd5e8
haznai <aitakprojects@gmail.com>
2021-08-05 13:14:20 +0200

    working on functions
    chapter 3: functions

--- 03_functions.js
+++ 03_functions.js
@@ -1,6 +1,21 @@
-// Function Type Expression
+// Function Type Expression 
 // accepts other functions as input => higher order function
 function greeter(fn) {
     fn("hello world");
 }
-greeter(function (a) { return console.log(a); });
+// arrow function (lambda function) passed to greeter
+greeter(function (s) { return console.log(s); });
+// generic function with constraints
+function printLength(sequence) {
+    console.log(sequence.length);
+}
+printLength("waddup");
+printLength([1, 2, 3]);
+function printProperties(_a) {
+    var a = _a.a, b = _a.b;
+    console.log("from the printProperties function: a: " + a + ", b: " + b);
+}
+var bar = { a: true, b: false };
+var a = bar.a, b = bar.b;
+console.log("a: " + a + ", b: " + b);
+printProperties(bar);
--- 03_functions.ts
+++ 03_functions.ts
@@ -1,9 +1,34 @@
-// Function Type Expression
+// Function Type Expression 
 // accepts other functions as input => higher order function
 function greeter(fn: (a: string) => void) 
 {
     fn("hello world")
 }
-
 // arrow function (lambda function) passed to greeter
-greeter((a)=> console.log(a))
+greeter((s)=> console.log(s))
+
+// generic function with constraints
+function printLength<Type extends {length:number}>(sequence: Type)
+{
+    console.log(sequence.length)
+}
+printLength("waddup")
+printLength([1, 2, 3])
+
+// parameter destructing
+type foo =
+{
+    a: boolean;
+    b: boolean;
+}
+
+function printProperties({a, b}:foo)
+{
+    console.log(`from the printProperties function: a: ${a}, b: ${b}`)
+}
+
+let bar: foo = {a: true, b:false}
+let {a, b} = bar
+console.log(`a: ${a}, b: ${b}`)
+printProperties(bar)
+
